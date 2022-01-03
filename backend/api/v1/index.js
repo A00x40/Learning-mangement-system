3eba41f853cc0d8b62cd116ce745ee6b2a14dae0
A00x40 <Ahmedattia621@yahoo.com>
2022-01-03 10:52:36 +0200

    Added routes index.js

--- backend/api/v1/index.js
+++ backend/api/v1/index.js
@@ -2,20 +2,14 @@ const express = require('express');
 
 const userRoute = require("./user.route");
 const courseRoute = require("./course.route");
+const courseRoute = require("./quiz.route");
 
-/*
-import authRoute from "./auth.route";
-import quizRoute from "./quiz.route";
-*/
 
 const router = express.Router();
 
 router.use("/user", userRoute);
 router.use("/course", courseRoute)
-/*
-router.use("/auth",authRoute);
 router.use("/quiz",quizRoute);
-;
-*/
+
 
 module.exports = router;
